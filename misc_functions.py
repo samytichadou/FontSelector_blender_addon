aabab8caeb62f6b83db7b1cd9d083e381fd95a38
samytichadou <samytichadou@gmail.com>
2019-01-14 22:34:01 +0100

    add create prefs function

--- misc_functions.py
+++ misc_functions.py
@@ -1,10 +1,11 @@
 import bpy
+import os
 
 from .preferences import get_addon_preferences
 
 
 # suppress filepath
-def fontselector_suppress_fp(index):
+def fontselector_suppress_fp(index) :
     #get addon prefs
     addon_preferences = get_addon_preferences()
     fplist = addon_preferences.font_folders
@@ -13,5 +14,10 @@ def fontselector_suppress_fp(index):
     #operator refresh fonts if list created
 
 # export menu
-def menu_export_favorites(self, context):
+def menu_export_favorites(self, context) :
     self.layout.operator('fontselector.export_favorites', text="Favorite Fonts export", icon='FILE_FONT')
+
+# create pref folder if doesn't exist
+def create_prefs_folder() :
+    if os.path.isdir(get_addon_preferences().prefpath) == False :
+        os.makedirs(prefpath)
