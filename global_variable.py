2d1c443560cc04aecc6d24d93f57d7687803822f
samytichadou <samytichadou@gmail.com>
2019-01-16 21:33:43 +0100

    add files nomenclature to glob var

--- global_variable.py
+++ global_variable.py
@@ -1,1 +1,8 @@
-extensions = [".otf", ".otc", ".ttf", ".ttc", ".tte", ".pfb", ".dfont", ".OTF", ".OTC", ".TTF", ".TTC", ".TTE", ".DFONT", ".PFB"]        
+extensions = [".otf", ".otc", ".ttf", ".ttc", ".tte", ".pfb", ".dfont", ".OTF", ".OTC", ".TTF", ".TTC", ".TTE", ".DFONT", ".PFB"]
+
+#font selector files
+fav_list = "fontselector_favorites"
+font_list = "fontselector_fontlist"
+filter_list = "fontselector_filter"
+subdir_list = "fontselector_subdir"
+size_file = "fontselector_size_"
