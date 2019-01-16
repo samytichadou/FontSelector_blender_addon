ca071cd98f3a86fdd9a33c0fb332feaa77c15b49
samytichadou <samytichadou@gmail.com>
2019-01-17 00:21:07 +0100

    syntax and optimization

--- operator_add_filtered_font.py
+++ operator_add_filtered_font.py
@@ -3,7 +3,9 @@ import os
 import csv
 
 from .preferences import get_addon_preferences
+from .misc_functions import absolute_path, create_dir
 
+from global_variable import filter_list
 
 class FontSelectorAddFiltered(bpy.types.Operator):
     bl_idname = "fontselector.add_filtered"
@@ -13,48 +15,44 @@ class FontSelectorAddFiltered(bpy.types.Operator):
     
     @classmethod
     def poll(cls, context):
-        #get addon prefs
         addon_preferences = get_addon_preferences()
-        new=addon_preferences.prefs_filter
-        return new!=''
+        new = addon_preferences.prefs_filter
+        return new != ''
     
     def execute(self, context):
         addon_preferences = get_addon_preferences()
         new = addon_preferences.prefs_filter
-        prefs = addon_preferences.prefs_folderpath
-        prefpath = os.path.abspath(bpy.path.abspath(prefs))
-        preffilter = os.path.join(prefpath, "fontselector_filter")
+        prefpath = absolute_path(addon_preferences.prefs_folderpath)
+        preffilter = os.path.join(prefpath, filter_list)
         
         filterlist=[]
         
         #get filtered
-        if os.path.isdir(prefpath)==True:
-            if os.path.isfile(preffilter)==True:
-                with open(preffilter, 'r', newline='') as csvfile:
-                    line = csv.reader(csvfile, delimiter='\n')
-                    for l in line:
-                        l1=str(l).replace("[", "")
-                        l2=l1.replace("]", "")
-                        l3=l2.replace("'", "")
-                        l4=l3.replace('"', "")
-                        filterlist.append(l4)
+        create_dir(prefpath)
+        if os.path.isfile(preffilter)==True:
+            with open(preffilter, 'r', newline='') as csvfile:
+                line = csv.reader(csvfile, delimiter='\n')
+                for l in line:
+                    l1=str(l).replace("[", "")
+                    l2=l1.replace("]", "")
+                    l3=l2.replace("'", "")
+                    l4=l3.replace('"', "")
+                    filterlist.append(l4)
         
         #add new one
-        chk=0
-        if new!="":
-            for f in filterlist:
-                if new==f:
-                    chk=1
-            if chk==0:
-                filterlist.append(new)
-            else:
-                info = new + ' already in Filtered Fonts'
-                self.report({'INFO'}, info)
+        chk_existing = 0
+        for f in filterlist :
+            if new == f :
+                chk_existing = 1
+                break
+        if chk_existing == 0 :
+            filterlist.append(new)
+        else:
+            info = new + ' already in Filtered Fonts'
+            self.report({'INFO'}, info)
         
         #create external file
-        if len(filterlist)!=0 and chk==0:
-            if os.path.isdir(prefpath)==False:
-                os.makedirs(prefpath)
+        if len(filterlist) != 0 and chk_existing == 0 :
             nfile = open(preffilter, "w")
             for f in filterlist:
                 nfile.write(f+"\n")
