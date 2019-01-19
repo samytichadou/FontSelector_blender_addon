a8b0343f3ebe56bfed44ad85e0710cbcd6768cac
samytichadou <samytichadou@gmail.com>
2019-01-19 02:41:37 +0100

    working progress bar

--- operator_modal_test.py
+++ operator_modal_test.py
@@ -7,16 +7,47 @@ import bgl
 from .misc_functions import get_all_font_files
 
 path = r"C:\Windows\Fonts"
+count = 0
+total = 0
 
+# draw box in open gl function
+def draw_box(x, y, w, h, color):
+    bgl.glColor4f(*color)
+    bgl.glEnable(bgl.GL_BLEND)
+    bgl.glBegin(bgl.GL_QUADS)
+    
+    bgl.glVertex2f(x + w, y + h)
+    bgl.glVertex2f(x, y + h)
+    bgl.glVertex2f(x, y)
+    bgl.glVertex2f(x + w, y)
+    bgl.glEnd()
+
+# callback for loading bar in 3D view 
 def draw_callback_loading(self, context): 
-    print("drawing")
+    bar_thickness = 30
+
+    # Progress Bar
+    width = context.area.width
+    x = 0
+    y = 0
+    completion = count / total
+    size = int(width * completion)
+
+    draw_box(x, y, size, bar_thickness, (1.0, 1.0, 1.0, 0.15))
+
+    # Text
+    bgl.glColor4f(1.0, 1.0, 1.0, 1.0)
     font_id = 0  # XXX, need to find out how best to get this.
+    text = "Fonts Loading"
+    xfont = width / 2 - 60
+    yfont = 10
+    blf.position(font_id, xfont, yfont, 0)
+    blf.size(font_id, 18, 72)
+    blf.draw(font_id, text)
 
-    # draw some text
-    blf.position(font_id, 15, 30, 0)
-    blf.size(font_id, 20, 72)
-    blf.draw(font_id, "Loading Test")
+    bgl.glEnd()
 
+# refresh operator modal
 class FontSelectorModalTest(bpy.types.Operator):
     bl_idname = "fontselector.modal_test"
     bl_label = "Modal Test"
@@ -25,17 +56,18 @@ class FontSelectorModalTest(bpy.types.Operator):
     _timer = None
 
     font_list = []
-    total = 0
-    count = 0
 
     def __init__(self):
+        global total
+
         for font in get_all_font_files(path) :
             self.font_list.append(font)
-        self.total = len(self.font_list)
+        total = len(self.font_list)
         print("Start")
 
     def modal(self, context, event):
-        ### TODO ### Change le space view pour mettre dans la barre d'info
+        global count
+
         # redraw info area
         try:
             for area in context.screen.areas:
@@ -54,10 +86,9 @@ class FontSelectorModalTest(bpy.types.Operator):
             try :
                 self._updating = True   
                 #font treatment
-                print(self.font_list[self.count])
-                print(str(self.count+1)+"/"+str(self.total))
-                self.count += 1
-
+                print(self.font_list[count])
+                print(str(count+1)+"/"+str(total))
+                count += 1
                 self._updating = False
                 return {'PASS_THROUGH'}
             except IndexError :
@@ -70,20 +101,35 @@ class FontSelectorModalTest(bpy.types.Operator):
 
     def execute(self, context):
         wm = context.window_manager
-        # the arguments we pass the the callback
+        # the arguments we pass the callback
         args = (self, context)
-        ### TODO ### Change le space view pour mettre dans la barre d'info
+        self._timer = wm.event_timer_add(0.001, context.window)
         self._handle = bpy.types.SpaceView3D.draw_handler_add(draw_callback_loading, args, 'WINDOW', 'POST_PIXEL')
-        self._timer = wm.event_timer_add(0.01, context.window)
         wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
     def cancel(self, context):
+        bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
         self.report({'INFO'}, "CANCEL")
         print('cancel')
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
 
+        global total
+        global count
+        total = 0
+        count = 0
+        self.font_list.clear()
+
     def finish(self, context):
+        bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
         self.report({'INFO'}, "FINISHED")
         print('finished')
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
+
+        global total
+        global count
+        total = 0
+        count = 0
+        self.font_list.clear()
