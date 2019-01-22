56c55007a1af43cd3d12e9b7894ad64c6671d9de
samytichadou <samytichadou@gmail.com>
2019-01-22 21:06:57 +0100

    improving modal

--- operators/modal_test.py
+++ operators/modal_test.py
@@ -4,11 +4,14 @@ import time
 import blf
 import bgl
 
-from ..misc_functions import get_all_font_files
+from ..misc_functions import get_all_font_files, create_dir, absolute_path, clear_collection, get_size
+from ..preferences import get_addon_preferences
 
 path = r"C:\Windows\Fonts"
 count = 0
 total = 0
+
+### UI ###
 
 # draw box in open gl function
 def draw_box(x, y, w, h, color):
@@ -51,22 +54,70 @@ def draw_callback_loading(self, context):
 
     bgl.glEnd()
 
+### MODAL ###
+
 # refresh operator modal
 class FontSelectorModalTest(bpy.types.Operator):
     bl_idname = "fontselector.modal_test"
     bl_label = "Modal Test"
 
+
     _updating = False
     _timer = None
 
     font_list = []
+    subdirectories = []
+    filter_list = []
+
+    data_font_list = bpy.data.fonts
+
+    @classmethod
+    def poll(cls, context):
+        addon_preferences = get_addon_preferences()
+        fontcheck = []      
+        fplist = addon_preferences.font_folders
+        try :
+            for f in fplist :
+                absolute_folder = absolute_path(f.folderpath)
+                for font in get_all_font_files(absolute_folder) :
+                    fontcheck.append(font)
+        except IndexError :
+            pass
+        return len(fontcheck)>0
+
 
     def __init__(self):
+
         global total
 
-        for font in get_all_font_files(path) :
-            self.font_list.append(font)
+        addon_preferences = get_addon_preferences()
+        fplist = addon_preferences.font_folders
+        prefpath = absolute_path(addon_preferences.prefs_folderpath)
+        collection_font_list = bpy.data.window_managers['WinMan'].fontselector_list
+
+        for folder in fplist :
+            absolute_folder = absolute_path(folder.folderpath)
+            fontpath_list, subdir_list = get_all_font_files(absolute_folder)
+            for font in fontpath_list :
+                self.font_list.append(font)
+            for subdir in subdir_list :
+                self.subdirectories.append(subdir)
         total = len(self.font_list)
+
+        #get filtered font
+
+        #create subdir list
+
+        #clean unused
+        if len(self.data_font_list) > 0:
+            bpy.ops.fontselector.remove_unused()
+        
+        #check if external folder exist and create it if not
+        create_dir(prefpath)
+        
+        #clear list
+        #clear_collection(collection_font_list)      
+
         print("Start")
 
         ### TODO ### turn relevant json files into old
@@ -77,9 +128,9 @@ class FontSelectorModalTest(bpy.types.Operator):
         # redraw area
         try:
             for area in context.screen.areas:
-                area.tag_redraw()
-#                if area.type == 'VIEW_3D':
-#                    area.tag_redraw()
+                #area.tag_redraw()
+                if area.type == 'VIEW_3D':
+                    area.tag_redraw()
         except AttributeError:
             pass
 
@@ -92,9 +143,27 @@ class FontSelectorModalTest(bpy.types.Operator):
         elif event.type == 'TIMER' and not self._updating :
             try :
                 self._updating = True   
+
                 #font treatment
-                print(self.font_list[count])
-                print(str(count+1)+"/"+str(total))
+                chk_local_dupe = 0
+                path, subdir, name = self.font_list[count]
+                for filtered in self.filter_list :
+                    if name == filtered :
+                        chk_local_dupe = 1
+                        print(str(count+1) + "/" + str(total) + " fonts treated --- " + name + " filtered out")
+                        break
+                if chk_local_dupe == 0 :
+                    try:
+                        bpy.data.fonts.load(filepath = path)
+                        
+                        print(str(count+1) + "/" + str(total) + " fonts treated --- " + name + " imported")
+                    except RuntimeError:
+                        self.filterlist.append(name)
+                        print(str(count+1) + "/" + str(total) + " fonts treated --- " + name + " corrupted, filtered out")
+
+                #print(self.font_list[count])
+                #print(str(count+1)+"/"+str(total))
+
                 count += 1
                 self._updating = False
                 return {'PASS_THROUGH'}
@@ -111,16 +180,12 @@ class FontSelectorModalTest(bpy.types.Operator):
         # the arguments we pass the callback
         args = (self, context)
         self._timer = wm.event_timer_add(0.001, context.window)
-        self._handle1 = bpy.types.SpaceView3D.draw_handler_add(draw_callback_loading, args, 'WINDOW', 'POST_PIXEL')
-        #self._handle2 = bpy.types.SpaceOutliner.draw_handler_add(draw_callback_loading, args, 'WINDOW', 'POST_PIXEL')
-        #self._handle3 = bpy.types.SpaceProperties.draw_handler_add(draw_callback_loading, args, 'WINDOW', 'POST_PIXEL')
+        self._handle = bpy.types.SpaceView3D.draw_handler_add(draw_callback_loading, args, 'WINDOW', 'POST_PIXEL')
         wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
     def cancel(self, context):
-        bpy.types.SpaceView3D.draw_handler_remove(self._handle1, 'WINDOW')
-        #bpy.types.SpaceOutliner.draw_handler_remove(self._handle2, 'WINDOW')
-        #bpy.types.SpaceProperties.draw_handler_remove(self._handle3, 'WINDOW')
+        bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
         self.report({'INFO'}, "CANCEL")
         print('cancel')
         wm = context.window_manager
@@ -130,14 +195,12 @@ class FontSelectorModalTest(bpy.types.Operator):
         global count
         total = 0
         count = 0
-        self.font_list.clear()
+        del self.font_list[:]
 
         ### TODO ### recover old json files
 
     def finish(self, context):
-        bpy.types.SpaceView3D.draw_handler_remove(self._handle1, 'WINDOW')
-        #bpy.types.SpaceOutliner.draw_handler_remove(self._handle2, 'WINDOW')
-        #bpy.types.SpaceProperties.draw_handler_remove(self._handle3, 'WINDOW')
+        bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
         self.report({'INFO'}, "FINISHED")
         print('finished')
         wm = context.window_manager
@@ -147,6 +210,6 @@ class FontSelectorModalTest(bpy.types.Operator):
         global count
         total = 0
         count = 0
-        self.font_list.clear()
+        del self.font_list[:]
 
         ### TODO ### delete json old files
