9525e2af3cd8b1b2d3235b5ba72a5c7fec38bad8
samytichadou <samytichadou@gmail.com>
2019-01-23 02:23:29 +0100

    add json format functions

--- json_functions.py
+++ json_functions.py
@@ -1,21 +1,46 @@
 import bpy
 import json
 
-def create_clip_json(path, data) :
+def create_json_file(datas, path) :
     with open(path, "w") as write_file :
-        json.dump(data, write_file)
+        json.dump(datas, write_file)
 
-def format_strip_infos(font) :
-    datas = {
-        "fonts": [{
-            "name": font.name,
-            "filepath": font.filepath,
-        }]
-    }
+def initialize_json_datas () :
+    datas = {}
+    datas['size'] = []
+    datas['fonts'] = []
+    datas['subdirectories'] = []
+    datas['filtered'] = []
+    datas['favorites'] = []
+    return datas
+
+def add_fonts_json(datas, font_list) :
+    #datas = {}
+    #datas['font'] = []
+    for font in font_list :
+        datas['fonts'].append([{
+            "name" : font[0],
+            "filepath" : font[1],
+            "subdirectory" : font[2]
+        }])
+    
+    return datas
+
+def add_subdirectories_json(datas, subdir_list) :
+    for sub in subdir_list :
+        datas['subdirectories'].append([{
+            "name" : sub[0]
+        }])
+    
+    return datas
+
+def add_size_json(datas, size) :
+    datas['size'] = size
+    
     return datas
 
 #read json
 def read_json(filepath):
     with open(filepath, "r") as read_file:
-        data = json.load(read_file)
-    return data
+        datas = json.load(read_file)
+    return datas
