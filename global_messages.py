618ca54e3f98ea76b92b4fe92ed78c01a104ac3c
samytichadou <samytichadou@gmail.com>
2019-01-23 21:11:35 +0100

    adding some messages

--- global_messages.py
+++ global_messages.py
@@ -1,4 +1,11 @@
 refresh_msg = "Font List Refreshed"
+cancel_refresh_msg = "Refresh List Cancelled"
+
 changes_msg = "Changes in Font Folders"
 no_changes_msg = "Font Folders up to date"
+
 folder_error_msg = "No valid Font Folder"
+
+settings_loaded_msg = "Settings Loaded"
+
+print_statement = "Font Selector ---"
