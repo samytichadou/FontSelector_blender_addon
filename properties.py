28b8310cba7abe92f686332bee4362c0558dff61
samytichadou <samytichadou@gmail.com>
2019-01-30 19:23:26 +0100

    add fake user properties

--- properties.py
+++ properties.py
@@ -5,13 +5,14 @@ from .update_functions import update_save_favorites
 
 class FontSelectorFontList(bpy.types.PropertyGroup) :
     '''name = StringProperty() '''
-    filepath = bpy.props.StringProperty(name = "filepath")
-    missingfont = bpy.props.BoolProperty(name = "missingfont", default = False)
-    favorite = bpy.props.BoolProperty(name = "favorite",
+    filepath = bpy.props.StringProperty(name = "File Path")
+    missingfont = bpy.props.BoolProperty(name = "Missing Font", default = False)
+    favorite = bpy.props.BoolProperty(name = "Favorite",
                                     default = False, 
                                     update = update_save_favorites,
                                     description = "Mark/Unmark as Favorite Font")
-    subdirectory = bpy.props.StringProperty(name="subdirectory")
+    subdirectory = bpy.props.StringProperty(name="Subdirectory")
+    fake_user = bpy.props.BoolProperty(name = "Fake User", default = False)
     
 class FontSelectorFontSubs(bpy.types.PropertyGroup) :
     '''name = StringProperty() '''
