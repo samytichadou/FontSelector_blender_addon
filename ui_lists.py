b5f75723c69bc06eb951c26f39fb6d56b8f257c7
samytichadou <samytichadou@gmail.com>
2019-07-02 17:40:14 +0200

    search bar improvement

--- ui_lists.py
+++ ui_lists.py
@@ -20,10 +20,11 @@ class FONTSELECTOR_UL_uilist(bpy.types.UIList):
 
     subdirectories_sorting : bpy.props.BoolProperty(name = "Sort by Subdirectories", description = "Sort by Subdirectories")
 
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, flt_flag) :
-        wm = context.window_manager
+    def __init__(self):
+        wm = bpy.context.window_manager
         self.filter_name = wm.fontselector_search
-        
+
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, flt_flag) :       
         layout.use_property_split = True # Active single-column layout
     
         #text_index = bpy.context.active_object.data.fontselector_index
